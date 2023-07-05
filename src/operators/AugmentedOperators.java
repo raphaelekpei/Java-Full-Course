25578b39c48ec637c59aab4d28684a6d7cc38bfd
Raphael Ekpei <ekpeiraphael020@gmail.com>
2023-07-05 02:27:57 +0100

    commit message

---
+++ src/operators/AugmentedOperators.java
@@ -0,0 +1,28 @@
+package operators;
+
+public class AugmentedOperators {
+    public static void main(String[] args) {
+
+        int numberOne = 10;
+        numberOne += 2; // -> numberOne = numberOne + 2;
+        System.out.println(numberOne);
+
+        int numberTwo = 10;
+        numberTwo -= 2; // -> numberTwo = numberTwo - 2;
+        System.out.println(numberTwo);
+
+        int numberThree = 10;
+        numberThree *= 2; // -> numberThree = numberThree * 2;
+        System.out.println(numberThree);
+
+        int numberFour = 10;
+        numberFour /= 2; // -> number = number + 2;
+        System.out.println(numberFour);
+
+        int numberFive = 10;
+        numberFive %= 2; // -> number = number + 2;
+        System.out.println(numberFive);
+
+
+    }
+}
---
+++ src/operators/IncrementOperator.java
@@ -0,0 +1,18 @@
+package operators;
+
+public class IncrementOperator {
+
+    public static void main(String[] args) {
+        int number = 0;
+
+        System.out.println(number++);
+        System.out.println(number);
+
+        System.out.println();
+
+        int numberTwo = 0;
+        System.out.println(++numberTwo);
+        System.out.println(numberTwo);
+    }
+
+}
