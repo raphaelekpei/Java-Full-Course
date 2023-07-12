a439482a3218a688473575139f94ff9dbfcc5f86
Raphael Ekpei <ekpeiraphael020@gmail.com>
2023-07-12 20:03:29 +0100

    Commit 110014

--- src/com/raphael/oop/pojo/example_1/PersonObject.java
+++ src/com/raphael/oop/pojo/example_1/PersonObject.java
@@ -1,22 +1,22 @@
-package com.raphael.oop.pojo.example_1;
-
-public class PersonObject {
-    public static void main(String[] args) {
-
-        Address address = new Address(1, );
-        Cat cat1 = new Cat("ebuka", 4, "black");
-        Cat car1 = new Cat("ernest", 5,"colorless");
-        Cat[] cats = {cat1, car1};
-
-        Person person1 = new Person("John", "Alex", Gender.MALE, cats, );
-        System.out.println(person1);
-
-
-        Person person2 = new Person("John", "Alex", Gender.);
-        System.out.println(person2);
-
-
-        System.out.println(person2);
-
-    }
-}
+//package com.raphael.oop.pojo.example_1;
+//
+//public class PersonObject {
+//    public static void main(String[] args) {
+//
+//        Address address = new Address(1, );
+//        Cat cat1 = new Cat("ebuka", 4, "black");
+//        Cat car1 = new Cat("ernest", 5,"colorless");
+//        Cat[] cats = {cat1, car1};
+//
+//        Person person1 = new Person("John", "Alex", Gender.MALE, cats, );
+//        System.out.println(person1);
+//
+//
+//        Person person2 = new Person("John", "Alex", Gender.);
+//        System.out.println(person2);
+//
+//
+//        System.out.println(person2);
+//
+//    }
+//}
